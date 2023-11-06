fbc24198c12829e5862d38f1f2f840726cf24ccb
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-05 19:35:14 -0500

    Uploading building simple liker lab

--- main.js
+++ main.js
@@ -5,6 +5,36 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const error = document.querySelector('div')
+error.className = 'hidden'
+
+const love = document.querySelectorAll('.like-glyph')
+
+const like = addEventListener('click', function (e) {
+  console.log('I am working')
+  mimicServerCall()
+  .catch(() => {
+    error.classList.remove('hidden');
+    const errorMessage = document.getElementById('modal-message')
+    errorMessage.textContent = "Random server error. Try again."
+    setTimeout(() => {
+      error.className = 'hidden'
+    }, 3000);
+    console.log(error);
+  })
+  
+})
+
+love.forEach((heart) => {
+  heart.addEventListener('click', function(e) {
+  if (heart.textContent === EMPTY_HEART) {
+    heart.textContent = FULL_HEART;
+    heart.className = 'activated-heart';
+  } else if (heart.textContent === FULL_HEART) {
+    heart.textContent = EMPTY_HEART;
+    heart.className = 'like-glyph';
+  }});
+})
 
 
 //------------------------------------------------------------------------------
